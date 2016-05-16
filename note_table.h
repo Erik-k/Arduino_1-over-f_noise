75b5e242fdbfc7891a1fc3fc5638bba2fb0878c3
Erik <enk@bu.edu>
2016-05-15 17:29:24 -0700

    Letting user select from an array of arrays

---
+++ note_table.h
@@ -0,0 +1,103 @@
+// -----------------------------------------------------------
+// Note Table
+// Public Constants
+// -----------------------------------------------------------
+
+const int ST = 0; //Stop
+const int NB0 = 31;
+const int NC1 = 33;
+const int NC1S = 35;
+const int ND1 = 37;
+const int ND1S = 39;
+const int NE1 = 41;
+const int NF1 = 44;
+const int NF1S = 46;
+const int NG1 = 49;
+const int NG1S = 52;
+const int NA1 = 55;
+const int NA1S = 58;
+const int NB1 = 62;
+
+const int NC2 = 65;
+const int NC2S = 69;
+const int ND2 = 73;
+const int ND2S = 78;
+const int NE2 = 82;
+const int NF2 = 87;
+const int NF2S = 93;
+const int NG2 = 98;
+const int NG2S = 104;
+const int NA2 = 110;
+const int NA2S = 117;
+const int NB2 = 123;
+
+const int NC3 = 131;
+const int NC3S = 139;
+const int ND3 = 147;
+const int ND3S = 156;
+const int NE3 = 165;
+const int NF3 = 175;
+const int NF3S = 185;
+const int NG3 = 196;
+const int NG3S = 208;
+const int NA3 = 220;
+const int NA3S = 233;
+const int NB3 = 247;
+
+const int NC4 = 262;
+const int NC4S = 277;
+const int ND4 = 294;
+const int ND4S = 311;
+const int NE4 = 330;
+const int NF4 = 349;
+const int NF4S = 370;
+const int NG4 = 392;
+const int NG4S = 415;
+const int NA4 = 440;
+const int NA4S = 466;
+const int NB4 = 494;
+
+const int NC5 = 523;
+const int NC5S = 554;
+const int ND5 = 587;
+const int ND5S = 622;
+const int NE5 = 659;
+const int NF5 = 698;
+const int NF5S = 740;
+const int NG5 = 784;
+const int NG5S = 831;
+const int NA5 = 880;
+const int NA5S = 932;
+const int NB5 = 988;
+
+const int NC6 = 1047;
+const int NC6S = 1109;
+const int ND6 = 1175;
+const int ND6S = 1245;
+const int NE6 = 1319;
+const int NF6 = 1397;
+const int NF6S = 1480;
+const int NG6 = 1568;
+const int NG6S = 1661;
+const int NA6 = 1760;
+const int NA6S = 1865;
+const int NB6 = 1976;
+
+const int NC7 = 2093;
+const int NC7S = 2217;
+const int ND7 = 2349;
+const int ND7S = 2489;
+const int NE7 = 2637;
+const int NF7 = 2794;
+const int NF7S = 2960;
+const int NG7 = 3136;
+const int NG7S = 3322;
+const int NA7 = 3520;
+const int NA7S = 3729;
+const int NB7 = 3951;
+
+const int NC8 = 4186;
+const int NC8S = 4435;
+const int ND8 = 4699;
+const int ND8S = 4978;
+
